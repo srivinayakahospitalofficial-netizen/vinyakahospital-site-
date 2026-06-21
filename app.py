7e124b88ecc2e7ee1827c22f90c94dea0c6839f6
Sri Vinayaka Hospital <srivinayakahospitalofficial@gmail.com>
2026-06-21 14:10:59 +0530

    Fix Render port

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
 BOT_TOKEN = "8820960604:AAFxAf8cxPeOBNUOoyUSRhqrMH3CJhz95mU"
 CHAT_ID = "-1003967286332"
 
+
+@app.route("/")
+def home():
+    return "API Working"
+
+
 @app.route("/send-lead", methods=["POST"])
 def send_lead():
 
-    data = request.json
+    data = request.get_json()
 
     name = data.get("name", "")
     phone = data.get("phone", "")
@@ -34,13 +41,6 @@ def send_lead():
     return jsonify({"success": True})
 
 
-@app.route("/")
-def home():
-    return "API Working"
-
-
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
